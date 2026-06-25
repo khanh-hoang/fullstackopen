3b870305ca30dcb882543148d211c9e3744e13c0
Khanh Hoang <khanh.hoang.swe@gmail.com>
2026-06-26 01:51:34 +0300

    fix: backend, end to end

--- part4/bloglist-backend/controllers/blogs.js
+++ part4/bloglist-backend/controllers/blogs.js
@@ -59,7 +59,7 @@ blogsRouter.delete('/:id', async (request, response) => {
 })
 
 blogsRouter.put('/:id', async (request, response) => {
-  const { title, author, url, likes } = request.body
+  const { title, author, url, likes, user } = request.body
 
   const blog = await Blog.findById(request.params.id)
   if (!blog) {
@@ -70,6 +70,7 @@ blogsRouter.put('/:id', async (request, response) => {
   blog.author = author
   blog.url = url
   blog.likes = likes
+  if (user) blog.user = user
 
   const updatedBlog = await blog.save()
   response.json(updatedBlog)
